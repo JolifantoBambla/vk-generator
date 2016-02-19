0e0fff7d1f4834aee36d090ca2c3bda407169fba
Bart Botta <00003b@gmail.com>
2016-02-18 23:55:10 -0600

    starting vkinfo example

---
+++ examples/vkinfo.lisp
@@ -0,0 +1,18 @@
+(in-package #:vk)
+
+(defvar *instance* (create-instance))
+
+(let ((devices (enumerate-physical-devices *instance*)))
+  (format t "~&got ~d devices~%" (length devices))
+  (loop for device in devices
+        for i from 0
+        for props = (get-physical-device-properties device)
+        do (format t "device ~d: ~a~%"
+                   i (foreign-string-to-lisp (getf props :device-name)))
+           (format t "  limits:~%    ~{~s ~s~^~%    ~}~%" (getf props :limits))
+           (remf props :limits)
+           (format t "  properties:~%    ~{~s ~s~^~%    ~}~%" props)
+           (format t "  features:~%    ~{~s ~S~^~%    ~}~%"
+                   (get-physical-device-features device))))
+
+(%vk::destroy-instance *instance* (null-pointer))
