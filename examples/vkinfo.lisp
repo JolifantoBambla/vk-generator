fedb28afcfc1a7451e1d3e6aacbc3b2dc4abccc7
Bart Botta <00003b@gmail.com>
2016-03-22 11:39:20 -0700

    add some more info to vkinfo

--- examples/vkinfo.lisp
+++ examples/vkinfo.lisp
@@ -13,10 +13,16 @@
             for props = (get-physical-device-properties device)
             do (format t "device ~d: ~a~%"
                        i (getf props :device-name))
+            #+os-windows
+             (format t "presentation support: ~s~%"
+                     (loop for f below (length (get-physical-device-queue-family-properties device))
+                             collect (get-physical-device-win32-presentation-support-khr device f)))
                (format t "device layers: ~s~%"
                        (enumerate-device-layer-properties device))
                (format t "device extensions: ~s~%"
                        (enumerate-device-extension-properties device ""))
+               (format t "queue families: ~s~%"
+                       (get-physical-device-queue-family-properties device))
                (format t "  limits:~%    ~{~s ~s~^~%    ~}~%" (getf props :limits))
                (remf props :limits)
                (format t "  properties:~%    ~{~s ~s~^~%    ~}~%" props)
