ee32bf97e5eb040257d656685bf216fe62f85dbb
Lukas Herzberger <herzberger.lukas@gmail.com>
2021-02-12 11:33:28 +0100

    fix parsing of name data modifiers

--- src/vk-spec/parse.lisp
+++ src/vk-spec/parse.lisp
@@ -20,41 +20,105 @@
   (let ((str (xps node)))
     (and str (string= str "true"))))
 
-(defun parse-modifiers (node)
+(defun parse-modifiers (modifiers)
+  "Parses a string of modifiers for a name node into a list of ARRAY-SIZES and a list of BIT-COUNT values.
+
+Known formats are:
+- bit count: \":<number>\"
+E.g.:
+<member>
+  <type>uint32_t</type>
+  <name>mask</name>
+  :8
+</member>
+
+- array size(s): \"[<number>]+\"
+E.g one-dimensional array size:
+<param>
+  const
+  <type>float</type>
+  <name>blendConstants</name>
+  [4]
+</param>
+
+E.g. multidimensional array sizes:
+<member>
+  <type>float</type>
+  <name>matrix</name>
+  [3][4]
+</member>
+
+Ignored formats are:
+- \")\"
+- \";\"
+- array size of a struct member referenced by a constant: \"[\"
+E.g.:
+<member>
+  <type>char</type>
+  <name>deviceName</name>
+  [
+  <enum>VK_MAX_PHYSICAL_DEVICE_NAME_SIZE</enum>
+  ]
+</member>
+
+Note:array sizes of struct members are handled by PARSE-STRUCT-MEMBER
+
+See also:
+- PARSE-NAME-DATA
+- NAME-DATA
+- PARSE-STRUCT-MEMBER
+- VULKAN-SPEC
+"
   (let ((array-sizes nil)
-        (bit-count nil))
-    (unless (xpath:node-set-empty-p node)
-      (let ((value (xps (xpath:evaluate "text()" node))))
-        (when (and value
-                   (> (length value) 0))
-          (cond
-            ((string= (first value) "[")
-             (let ((end-pos 0))
-               (loop while (not (= (+ end-pos) (length value)))
-                     do (let ((start-pos (position #\[ :start end-pos)))
-                          (assert start-pos
-                                  () "could not find '[' in <~a>" value)
-                          (setf end-pos (position #\] :start start-pos))
-                          (assert end-pos
-                                  () "could not find ']' in <~a>" value)
-                          (assert (<= (+ start-pos 2) end-pos)
-                                  () "missing content between '[' and ']' in <~a>" value)
-                          (push (subseq value (1+ start-pos) (- end-pos start-pos 1))
-                                array-sizes)))))
-            ((string= (first value) ":")
-             (setf bit-count (cdr value)))
-            (t
-             (assert (or (string= (first value) ";")
-                         (string= (first value) ")"))
-                     () "unknown modifier <~a>" value))))))
+        (bit-count nil)
+        (value modifiers))
+    (when (and value
+               (> (length value) 0))
+      (let ((first-char (subseq value 0 1)))
+        (cond
+          ((and (not (string= value "["))
+                 (string= first-char "["))
+           (let ((end-pos 0))
+             (loop while (not (= (1+ end-pos) (length value)))
+                   do (let ((start-pos (position #\[ value :start end-pos)))
+                        (assert start-pos
+                                () "could not find '[' in <~a>" value)
+                        (setf end-pos (position #\] value :start start-pos))
+                        (assert end-pos
+                                () "could not find ']' in <~a>" value)
+                        (assert (<= (+ start-pos 2) end-pos)
+                                () "missing content between '[' and ']' in <~a>" value)                       
+                        (push (subseq value (1+ start-pos) end-pos)
+                              array-sizes)))))
+          ((string= first-char ":")
+           (setf bit-count (subseq value 1)))
+          (t
+           (assert (or (string= first-char "[")
+                       (string= first-char ";")
+                       (string= first-char ")"))
+                   () "unknown modifier <~a>" value)))))
     (values array-sizes bit-count)))
 
 (defun parse-name-data (node)
-  "TODO"
+  "Parses name data from a node into a NAME-DATA instance.
+
+E.g.:
+<param>
+  const
+  <type>float</type>
+  <name>blendConstants</name>
+  [4]
+</param>
+
+See also:
+- PARSE-MODIFIERS
+- NAME-DATA
+- VULKAN-SPEC
+"
   ;; todo: check attributes
   (let ((name (xps (xpath:evaluate "name" node))))
     (multiple-value-bind (array-sizes bit-count)
-        (parse-modifiers (xpath:evaluate "name/following-sibling" node))
+        (parse-modifiers (xps (xpath:evaluate "name/following-sibling::text()" node)))
       (make-instance 'name-data
                      :name name
                      :array-sizes array-sizes
